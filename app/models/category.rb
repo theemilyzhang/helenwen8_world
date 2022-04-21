fecd6b2d306562eaa3b65f621dd921dc94959920
Emily Zhang <ejzhang@andrew.cmu.edu>
2022-04-21 19:34:37 -0400

    setup article model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,8 @@
 class Category < ApplicationRecord
+	has many :articles
 	has_many :photos
 
 	scope :active, 			 -> { where('active = ?', true) }
-  scope :alphabetical, -> { order('name') }
+	scope :alphabetical, -> { order('name') }
   
 end
